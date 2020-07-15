2adb92d962d0373763e9cb1242bcd50b826396f6
Sean Harrington <sean@never-not.com>
2020-07-15 15:01:48 -0400

    revert scroll breadcrumbs to full section tracking

--- src/pages/debug.js
+++ src/pages/debug.js
@@ -37,7 +37,7 @@ const Debug = ({ location }) => (
 
     <Box id="sections">
       <SplitSectionLongContent
-        flip
+        flip={false}
         id={"debug"}
         contentArray={[
           {
